718cd078277668ad8fe871f7edc0386d83890099
Gavin Stark <atthecodeface@gmail.com>
2024-12-24 15:20:36 +0000

    [regression] Improve struct tests

---
+++ regression/tests/struct/color.h
@@ -0,0 +1,8 @@
+/*t t_color
+ */
+typedef struct
+{
+    bit[3] r;
+    bit[3] g;
+    bit[3] b;
+} t_color;
---
+++ regression/tests/struct/point.h
@@ -0,0 +1,8 @@
+/*t t_point
+ */
+typedef struct
+{
+    bit[4] x;
+    bit[4] y;
+} t_point;
+
